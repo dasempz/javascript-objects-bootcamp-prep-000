2996d681f3f12800af78f8b29d6fc34371db5832
Andrea H <andhaveman@hotmail.com>
2019-03-21 15:30:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = 'Slowdive';
   return playlist;
 }
-function removeFromPlaylist(playlist, artistName){
+function removeFromPlaylist(playlist, artistName, songTitle){
   delete playlist.artistName;
   return playlist;
 }
