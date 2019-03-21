894c5625b3d03daac2d576d7fbc9d340a5390bd3
Andrea H <andhaveman@hotmail.com>
2019-03-21 15:06:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: '', songTitle: ''};
   
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['Phil Ochs'] = 'songTitle';
+  playlist['Phil Ochs'] = 'Slowdive';
   return playlist;
 }
